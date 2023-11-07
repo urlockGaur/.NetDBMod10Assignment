b64cad787508af744aa8712756baa1929025504e
Anthony <thewarparmoury@gmail.com>
2023-11-06 22:28:02 -0600

    Updated .Net version to 6.0 Updated EF versions to 7.0.13 Ran db update via cmd line Connected to bitsql.wctc.edu Menu setup WIP

--- Program.cs
+++ Program.cs
@@ -8,6 +8,94 @@ namespace EFTutorial
     {
         static void Main(string[] args)
         {
+
+
+            //Menu
+            while (true) {
+
+                var input = DisplayMenu();
+
+                if (input == "1")
+                {
+                    using (var db = new BlogContext())
+                    {
+                        Console.WriteLine("Blog List: ");
+
+                        var blogs = db.Blogs.ToList();
+                        if (blogs.Any())
+                        {
+                            foreach (var blogDisplay in blogs)
+                            {
+                                Console.WriteLine($"{blogDisplay.BlogId} {blogDisplay.Name}");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No blogs found. Please add blogs to the database.");
+                            Console.WriteLine("Press 'Enter' to navigate to back to the main menu.");
+                            Console.ReadLine();
+                            
+                        }
+                    }
+                }
+                else if (input == "2")
+                {
+                    Console.WriteLine("Hello!");
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            static string DisplayMenu()
+            {
+                Console.WriteLine("Blogs and Posts Assignment: Mod 10");
+                Console.WriteLine("--------------------------------------");
+
+                Console.WriteLine("Please select an option: ");
+                Console.WriteLine("1.) Display Blogs");
+                Console.WriteLine("2.) Add Blog");
+                Console.WriteLine("3.) Display Posts");
+                Console.WriteLine("4.) Add Post");
+                Console.WriteLine("Press q to quit application");
+                return Console.ReadLine();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             // 1. List Posts for Blog #1
             using (var db = new BlogContext()) 
             {
